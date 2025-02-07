0be7c888c7d5a54f7299b28dcd1ebfd459017ea8
Jason Botterill <jasonbotterill@hotmail.com>
2025-02-07 11:23:52 +0800

    Add more efficient door open

--- MCPI_Scripts/door.py
+++ MCPI_Scripts/door.py
@@ -14,15 +14,18 @@ def check_for_door():
             for dz in range(-2, 3):
                 # Get the block at each position
                 block_pos = (int(pos.x + dx), int(pos.y + dy), int(pos.z + dz))
-                block_type = mc.getBlock(block_pos[0], block_pos[1], block_pos[2])
+                block_with_data = mc.getBlockWithData(block_pos[0], block_pos[1], block_pos[2])
                 
                 # If it's a wooden door (block ID 64)
-                if block_type == 64:
-                    # Simply try to open the door
-                    # Try a few common door states that represent "open"
-                    for state in [4, 5, 6, 7]:
-                        mc.setBlock(block_pos[0], block_pos[1], block_pos[2], 64, state)
-                    return True
+                if block_with_data.id == 64:
+                    # Check if the door is closed (data values 0-3 indicate closed states)
+                    if block_with_data.data in [0, 1, 2, 3]:
+                        # Open the door.  The open state depends on the hinge and direction.
+                        # A door's data value will be 0-3 when closed, and 4-7 when open.
+                        # We can calculate the opened state by adding 4.
+                        new_data = block_with_data.data + 4
+                        mc.setBlock(block_pos[0], block_pos[1], block_pos[2], 64, new_data)
+                        return True
     return False
 
 # Just check once and exit
